5d9fabf798bbf3a3c2c2d6cc7a85a95209718160
jasontrigg0 <jasontrigg0@gmail.com>
2018-04-16 02:37:32 -0400

    pypi version 0.0.3

--- setup.py
+++ setup.py
@@ -2,14 +2,18 @@ from distutils.core import setup
 setup(
     name = 'python-db',
     packages = ['pydb'],
-    version = "0.0.2",
+    version = "0.0.3",
     description = 'Placeholder description',
     author = "Jason Trigg",
     author_email = "jasontrigg0@gmail.com",
     url = "https://github.com/jasontrigg0/python-db",
-    download_url = 'https://github.com/jasontrigg0/python-db/tarball/0.0.2',
+    download_url = 'https://github.com/jasontrigg0/python-db/tarball/0.0.3',
     scripts=['bin/db'],
-    install_requires=[],
+    install_requires=['mysqldb',
+                      'argparse',
+                      'jtutils',
+                      'python-csv'
+    ],
     keywords = [],
     classifiers = [],
 )
